26ced3bc2db000bd8d53c39e2a5eb951f8720a47
Danil Kushch <73854173+Reap3r@users.noreply.github.com>
2021-06-03 23:38:47 +0500

    Add files via upload

---
+++ Lab 18.9/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Vector.h"
+using namespace std;
+
+int main() {
+	int size;
+	cout << "Enter vector's size: ";
+	cin >> size;
+	Vector a(size);
+	cin >> a;
+	cout << "Your vector:" << endl;
+	cout << a;
+	cout << endl;
+	int n;
+	cout << "What element you want to change: ";
+	cin >> n;
+	cin >> a[n];
+	cout << "Your vector:" << endl;
+	cout << a;
+	cout << endl;
+	cout << "Enter value you want to increase your vector: ";
+	cin >> n;
+	a=a*n;
+	cout << "Your vector's size is " << a() << endl;
+	cout << "Your vector:" << endl;
+	cout << a;
+	cout << endl;
+	return 0;
+}
